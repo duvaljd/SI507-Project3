9c8d314bf04ee7d4d0ae46eeaf9cb1747efb3056
jd <duvaljd@umich.edu>
2017-10-19 18:02:41 -0400

    Parts 1 & 2 working

--- si507f17_project3_code.py
+++ si507f17_project3_code.py
@@ -9,6 +9,61 @@ import requests
 ## NOTE OF ADVICE:
 ## When you go to make your GitHub milestones, think pretty seriously about all the different parts and their requirements, and what you need to understand. Make sure you've asked your questions about Part 2 as much as you need to before Fall Break!
 
+stateNames_list = ["Arkansas", "California", "Michigan"]
+stateAbbrs = {
+'alabama':'al',
+'alaska':'ak',
+'arizona':'az',
+'arkansas':'ar',
+'california':'ca',
+'colorado':'co',
+'connecticut':'ct',
+'delaware':'de',
+'florida':'fl',
+'georgia':'ga',
+'hawaii':'hi',
+'idaho':'id',
+'illinois':'il',
+'indiana':'in',
+'iowa':'ia',
+'kansas':'ks',
+'kentucky':'ky',
+'louisiana':'la',
+'maine':'me',
+'maryland':'md',
+'massachusetts':'ma',
+'michigan':'mi',
+'minnesota':'mn',
+'mississippi':'ms',
+'missouri':'mo',
+'montana':'mt',
+'nebraska':'ne',
+'nevada':'nv',
+'new hampshire':'nh',
+'new jersey':'nj',
+'new mexico':'nm',
+'new york':'ny',
+'north carolina':'nc',
+'north dakota':'nd',
+'ohio':'oh',
+'oklahoma':'ok',
+'oregon':'or',
+'pennsylvania':'pa',
+'rhode island':'ri',
+'south carolina':'sc',
+'south dakota':'sd',
+'tennessee':'tn',
+'texas':'tx',
+'utah':'ut',
+'vermont':'vt',
+'virginia':'va',
+'washington':'wa',
+'west virginia':'wv',
+'wisconsin':'wi',
+'wyoming':'wy',
+'guam':'gu',
+'puerto rico':'pr',
+'virgin islands':'vi'}
 
 ######### PART 0 #########
 
@@ -38,72 +93,43 @@ getAltText("http://newmantaylor.com/gallery.html")
 ######### PART 1 #########
 
 # Get the main page data...
-try:
-	data = open("nps_gov_data.htm", "r")
-except:
-	data = requests.get("https://www.nps.gov/index.htm").text
-	f = open("nps_gov_data.htm", "w")
-	f.write(data)
-	f.close()
+def getNPS_data(returnData = True, makeSoup = False):
+	try:
+		nps_data = open("nps_gov_data.html", "rb").read()
+	except:
+		nps_data = requests.get("https://www.nps.gov/index.htm").text
+		f = open("nps_gov_data.html", "wb")
+		f.write(nps_data.encode("utf-8"))
+		f.close()
 
-def 
+	if makeSoup == True:
+		nps_soup = BeautifulSoup(nps_data, "html.parser")
+		menuData_soup = nps_soup.find('ul', attrs={"class":["dropdown-menu", "SearchBar-keywordSearch"]})
+		return menuData_soup
 
+	elif returnData == True:
+		return nps_data
 
+def getState_data(state, returnData = True):
+	try:
+		state_data = open("{}_data.html".format(state).lower(), "rb").read()
+	except:
+		menuData_soup = getNPS_data(False, True)
 
-# Try to get and cache main page data if not yet cached
-# Result of a following try/except block should be that
-# there exists a file nps_gov_data.html,
-# and the html text saved in it is stored in a variable 
-# that the rest of the program can access.
+		for link in menuData_soup.find_all("a"):
+			if link.text.lower() == "{}".format(state).lower():
+				print(link.get("href"))
+				state_data = requests.get("https://www.nps.gov{}".format(link.get("href"))).text
+				f = open("{}_data.html".format(state).lower(), "wb")
+				f.write(state_data.encode("utf-8"))
+				f.close
 
-# We've provided comments to guide you through the complex try/except, but if you prefer to build up the code to do this scraping and caching yourself, that is OK.
+	if returnData == True:
+		return state_data
 
-
-
-
-
-
-# Get individual states' data...
-
-# Result of a following try/except block should be that
-# there exist 3 files -- arkansas_data.html, california_data.html, michigan_data.html
-# and the HTML-formatted text stored in each one is available
-# in a variable or data structure 
-# that the rest of the program can access.
-
-# TRY: 
-# To open and read all 3 of the files
-
-# But if you can't, EXCEPT:
-
-# Create a BeautifulSoup instance of main page data 
-# Access the unordered list with the states' dropdown
-
-# Get a list of all the li (list elements) from the unordered list, using the BeautifulSoup find_all method
-
-# Use a list comprehension or accumulation to get all of the 'href' attributes of the 'a' tag objects in each li, instead of the full li objects
-
-# Filter the list of relative URLs you just got to include only the 3 you want: AR's, CA's, MI's, using the accumulator pattern & conditional statements
-
-
-# Create 3 URLs to access data from by appending those 3 href values to the main part of the NPS url. Save each URL in a variable.
-
-
-## To figure out what URLs you want to get data from (as if you weren't told initially)...
-# As seen if you debug on the actual site. e.g. Maine parks URL is "http://www.nps.gov/state/me/index.htm", Michigan's is "http://www.nps.gov/state/mi/index.htm" -- so if you compare that to the values in those href attributes you just got... how can you build the full URLs?
-
-
-# Finally, get the HTML data from each of these URLs, and save it in the variables you used in the try clause
-# (Make sure they're the same variables you used in the try clause! Otherwise, all this code will run every time you run the program!)
-
-
-# And then, write each set of data to a file so this won't have to run again.
-
-
-
-
-
-
+ak_data = getState_data("Arkansas")
+ca_data = getState_data("California")
+mi_data =getState_data("Michigan")
 
 ######### PART 2 #########
 
@@ -128,17 +154,41 @@ def
 
 ## Define your class NationalSite here:
 
+class NationalSite(object):
 
+	def __init__(self, parkSoup):
+		self.parkSoup = parkSoup
+		self.location = parkSoup.h4.text
+		self.name = parkSoup.h3.text
+		self.type = parkSoup.h2.text
+		self.description = parkSoup.p.text
 
+	def __str__(self):
+		return "{} | {}".format(self.name, self.location)
 
+	def __contains__(self, inParkName):
+		if str(inParkName).lower() in self.name.lower():
+			return True
+
+	def get_mailing_address(self):
+		urlSoup = self.parkSoup.select_one("a[href*=basic]")
+		data = requests.get(urlSoup.get("href")).text
+		basicInfoSoup = BeautifulSoup(data, "html.parser")
+		street = basicInfoSoup.find("span", itemprop = "streetAddress")
+		locality = basicInfoSoup.find("span", itemprop = "addressLocality")
+		region = basicInfoSoup.find("span", itemprop = "addressRegion")
+		code = basicInfoSoup.find("span", itemprop = "postalCode")
+
+		return(str("{} / {} / {} / {}").format(street.text, locality.text, region.text, code.text)) 
 
 ## Recommendation: to test the class, at various points, uncomment the following code and invoke some of the methods / check out the instance variables of the test instance saved in the variable sample_inst:
 
-# f = open("sample_html_of_park.html",'r')
-# soup_park_inst = BeautifulSoup(f.read(), 'html.parser') # an example of 1 BeautifulSoup instance to pass into your class
-# sample_inst = NationalSite(soup_park_inst)
-# f.close()
+f = open("sample_html_of_park.html",'r')
+soup_park_inst = BeautifulSoup(f.read(), 'html.parser') # an example of 1 BeautifulSoup instance to pass into your class
+sample_inst = NationalSite(soup_park_inst)
+f.close()
 
+sampleSite = NationalSite(soup_park_inst)
 
 ######### PART 3 #########
 
